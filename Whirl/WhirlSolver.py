6e2286246066a469210f63bc591625d4ba0bcc9d
Namaste Hacker <95855028+NamasteAbhi@users.noreply.github.com>
2024-09-26 12:28:15 +0530

    Update WhirlSolver.py

--- Whirl/WhirlSolver.py
+++ Whirl/WhirlSolver.py
@@ -1,1 +1,21 @@
+import requests
 
+RapidApiKey = ""  # Enter Your Rapid Key Here
+
+payload = {
+    "proxy": "username:password@host:port",  # Use Proxy In Same Format
+    "endpoint": "rc-verification16-normal-useast5.tiktokv.us",
+    "iid": "7383569631618991878",
+    "device_id": "7366359272542291461",
+    "region": "us",
+    "VerifyFp": ""  # For App No Need VerifyFp Token But For Web You Need
+}
+headers = {
+    "x-rapidapi-key": RapidApiKey,
+    "x-rapidapi-host": "bytedancecaptcha.p.rapidapi.com",
+    "Content-Type": "application/json"
+}
+
+response = requests.post("https://bytedancecaptcha.p.rapidapi.com/Whirl_Captcha/", json=payload, headers=headers)
+
+print(response.json())
